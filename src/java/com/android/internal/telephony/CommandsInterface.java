7fb6aa40384d3666bd5dfbe566f0102a987b44f8
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-11-30 19:30:08 +0700

    Add Samsung STK support (telephony part)
    Support for SMS-based SIM applications.
    Partially rewritten the original patch by Jüri Schultz to be less intrusive.
    
    Change-Id: I15d2de1781a2823e24941dd792db3d372578aa9f
    
    Conflicts:
    	src/java/com/android/internal/telephony/BaseCommands.java
    	src/java/com/android/internal/telephony/RIL.java
    
    Conflicts:
    
    	src/java/com/android/internal/telephony/CommandsInterface.java

--- src/java/com/android/internal/telephony/CommandsInterface.java
+++ src/java/com/android/internal/telephony/CommandsInterface.java
@@ -1686,4 +1686,12 @@ public interface CommandsInterface {
      * @return version of the ril.
      */
     int getRilVersion();
+
+    /**
+     * @hide
+     * samsung stk service implementation - set up registrant for sending
+     * sms send result from modem(RIL) to catService
+     */
+    void setOnCatSendSmsResult(Handler h, int what, Object obj);
+    void unSetOnCatSendSmsResult(Handler h);
 }
